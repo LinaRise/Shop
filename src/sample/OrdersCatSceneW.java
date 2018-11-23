960b3416c4b7252af873a8cb79325479ef146570
Alina Galeeva <alina2401g@gmail.com>
2018-11-23 12:54:24 +0300

    заполняем таблицы и подробную инфу в orderscatscene

--- src/sample/OrdersCatSceneW.java
+++ src/sample/OrdersCatSceneW.java
@@ -1,4 +1,127 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import sample.AnimationAndDecor.AnimationAndDecor;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class OrdersCatSceneW {
+
+
+    private ObservableList<Order> orderData = FXCollections.observableArrayList();
+    private ObservableList<Order> orderDetailData = FXCollections.observableArrayList();
+        @FXML
+        private TableView<Order> ordersTab;
+
+        @FXML
+        private TableColumn<?, ?> orderNum;
+
+        @FXML
+        private TableColumn<?, ?> fio;
+
+        @FXML
+        private Label orderNumLabel;
+
+        @FXML
+        private ComboBox stateBox;
+
+        @FXML
+        private TableView<Order> orderDetailTab;
+
+        @FXML
+        private TableColumn<?, ?> measures;
+
+    @FXML
+    private TableColumn<?, ?> telephone;
+
+    @FXML
+    private TextArea orderTextArea;
+
+    @FXML
+        private Label costLabel;
+
+    //метод выводящий сцену катлога заквзов
+    void orderCatSceneCall(){
+        Stage stage = new Stage();
+        stage.setTitle("Основное меню");
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("fxml/ordersCatSceneW.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            AnimationAndDecor.addIcon(stage, "sample/assets/needle.png");
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Файл ordersCatSceneW.fxml не найден " );
+            e.printStackTrace();
+        }Main.toClose(stage, "Нажав ОК Вы выйдете из основного меню ");
+    }
+
+    //метод для заполнения таблицы заказа из файла
+    private void orderTableFillIn(){
+
+        ArrayList<String> lineArray= null;
+        try {
+            lineArray = Order.ordersFileread("Orders.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+          int i=1;
+        for(String line:lineArray) {
+            String[] arrSplit = line.split("\\|",6);
+            System.out.println(Arrays.toString(arrSplit));
+            orderData.add(new Order(i,arrSplit[0],arrSplit[1],arrSplit[2],arrSplit[3],arrSplit[4],arrSplit[5]));
+            fio.setCellValueFactory(new PropertyValueFactory<>("fio"));
+            orderNum.setCellValueFactory(new PropertyValueFactory<>("orderNum"));
+            ordersTab.setItems(orderData);
+            i++;
+        }
+
+    }
+
+
+    private void orderDetailTableFillIn(Order order){
+        orderDetailTab.getItems().clear();
+        if (order != null) {
+            orderDetailData.add(new Order(order.getMeasures(),order.getTelephone()));
+            measures.setCellValueFactory(new PropertyValueFactory<>("measures"));
+            telephone.setCellValueFactory(new PropertyValueFactory<>("telephone"));
+            orderDetailTab.setItems(orderDetailData);
+            orderNumLabel.setText(String.valueOf(order.getOrderNum()));
+            stateBox.setValue(order.getState());
+            costLabel.setText(order.getCost());
+            orderTextArea.setText(order.getOrder());
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    @FXML
+    void initialize(){
+        orderTableFillIn();
+        ordersTab.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+            orderDetailTableFillIn(newValue));
+        }
+
 }
+
